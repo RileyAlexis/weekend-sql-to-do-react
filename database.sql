fdc9cdf5bfdd25cceb1093fc6def0dac1cbd0832
RileyAlexis <MxRileyAlexis@gmail.com>
2023-08-13 12:18:52 -0500

    SQL code written, database creatred

---
+++ database.sql
@@ -0,0 +1,32 @@
+CREATE TABLE tasklist (
+    "id" SERIAL PRIMARY KEY,
+    "title" varchar(255),
+    "start_date" date,
+    "start_time" time,
+    "due_date" date,
+    "due_time" time,
+    "repeatTask" varchar(64) ARRAY, 
+    "notes" text,
+    "url" text,
+    "priority" integer,
+    "list" integer,
+    "icon" varchar(64)
+)
+
+CREATE TABLE lists (
+    "id" SERIAL PRIMARY KEY,
+    "title" varchar(255),
+    "taskKeys" integer ARRAY
+)
+
+
+INSERT INTO tasklist (title, start_date, start_time, due_date, 
+            due_time, "repeatTask", notes, url, priority, list, icon)
+
+VALUES ('Buy more Candy', '20230814', '14:30:00-8:00', '20230814', 
+        '16:45:00-8:00', '{M, T, W, Th, F}', 'LIST::Red licorice:Caramels:Jelly Beans',
+        'www.moreCandy.com', '1', '1', '::heart::'
+        )
+
+INSERT INTO lists (title, "taskKeys")
+VALUES ('Candy List', '{1}')
